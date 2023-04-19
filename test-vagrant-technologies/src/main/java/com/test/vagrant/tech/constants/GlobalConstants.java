7d579b0171a4bc06dfed310581a248816dc099f3
Yogesh Aknurwar <yogesh.aknurwar@gmail.com>
2023-04-19 17:47:45 +0530

    feat: first test created

--- test-vagrant-technologies/src/main/java/com/test/vagrant/tech/constants/GlobalConstants.java
+++ test-vagrant-technologies/src/main/java/com/test/vagrant/tech/constants/GlobalConstants.java
@@ -3,6 +3,7 @@ package com.test.vagrant.tech.constants;
 public class GlobalConstants {
 	
 	public static final String DATA = "TeamRCB.json";
+	public static final int FOREIGN_PLAYERS_COUNT = 4;
 	
 
 }
--- test-vagrant-technologies/src/test/java/com/test/vagrant/tech/tests/ProblemStatement.java
+++ test-vagrant-technologies/src/test/java/com/test/vagrant/tech/tests/ProblemStatement.java
@@ -1,11 +1,16 @@
 package com.test.vagrant.tech.tests;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.json.simple.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import com.test.vagrant.tech.constants.GlobalConstants;
+import com.test.vagrant.tech.pages.Players;
 import com.test.vagrant.tech.pages.RoyalChallengersBangloreTeam;
 import com.test.vagrant.tech.utility.Util;
 
@@ -13,15 +18,26 @@ public class ProblemStatement extends Util {
 
 	private static Logger logger = LoggerFactory.getLogger(ProblemStatement.class);
 	GlobalConstants constant;
+
 	@Test
 	public void verifyForeignPlayersTeamSize() throws Exception {
 
 		JSONObject jsonObject = super.readJSONFile(GlobalConstants.DATA);
 
 		RoyalChallengersBangloreTeam teamDetails = RoyalChallengersBangloreTeam.fromJSON(jsonObject.toJSONString());
-		
-	
-		System.out.println("Details are "+teamDetails.getPlayer());
+
+		List<Players> players = teamDetails.getPlayer();
+		List<String> countries = new ArrayList<String>();
+
+		for (Players player : players) {
+			countries.add(player.getCountry());
+		}
+
+		countries.removeIf(country -> country.contains("India"));
+		System.out.println("Foreign countries are " + countries);
+
+		Assert.assertEquals(countries.size(), GlobalConstants.FOREIGN_PLAYERS_COUNT,
+				"Team does not have " + GlobalConstants.FOREIGN_PLAYERS_COUNT + " foreign players");
 
 	}
 }
